9ff93ee2a704aaa9139de2f740216c836ff9f7d1
Bryant Schultz <bryantschultz99@gmail.com>
2023-12-02 10:21:31 -0600

    Testing endpoint

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def userform():
     data = request.form
     user_id = data.get('user_id')
     channel_id = data.get("channel_id")
-    print(user_id)
+    print(data)
     client.chat_postEphemeral(channel=channel_id, user=user_id, text="Testing", blocks=USER_PORTAL)
     return Response(), 200
 
