8efd82d81fa923720ab87d0f7cfaa6a568b9ba6b
Bryant Schultz <bryantschultz99@gmail.com>
2024-08-02 16:54:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ def user_portal(body, ack, client, logger):
 def view_submission(ack, body, client, logger):
     ack()
     logger.info(body["view"]["state"]["values"])
-    print(type(body["view"]["state"]["values"]))
+    for value in body["view"]["state"]["values"].values():
+        print(value)
     # Extra Credit: Uncomment out this section
     # thank_you_channel = "your_channel_id"
     # user_text = body["view"]["state"]["values"]["my_block"]["my_action"]["value"]
