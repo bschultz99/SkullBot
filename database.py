fe98b88da5745097eddc0f709c5d55daf732cd07
Bryant Schultz <bryantschultz99@gmail.com>
2024-08-10 11:44:24 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 
 USER_TABLE = """
 CREATE TABLE IF NOT EXISTS users (
-    slack_id VARCHAR(255),
+    slack_id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255),
     membership VARCHAR(255)
     );
