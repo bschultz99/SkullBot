a4b3ec0d4d50118fa05b1dca9fa5a739ad9c8f17
Bryant Schultz <bryantschultz99@gmail.com>
2024-08-16 19:34:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def view_submission(ack, body, client, logger):
 def remove_user(ack, body, client, logger):
     ack()
     slack_id = body['view']['blocks'][0]['accessory']['options'][0]['value']
-    print(body['view']['blocks'])
+    print(body['view']['state'])
     print(slack_id)
     #cursor.execute(REMOVE_SELECTED_USER, (slack_id,))
     #conn.commit()
